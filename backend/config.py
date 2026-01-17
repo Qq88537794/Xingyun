74ce63119f3d17051453b056dd399e6b218be7c1
xxy <xianxingyuan520@outlook.com>
2026-01-17 21:21:11 +0800

    文档更新

--- backend/config.py
+++ backend/config.py
@@ -15,7 +15,7 @@ class Config:
     DB_PORT = os.environ.get('DB_PORT', '3306')
     DB_NAME = os.environ.get('DB_NAME', 'xingyun')
     DB_USER = os.environ.get('DB_USER', 'root')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', '你的MySQL密码')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'XxY110110!')
     
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
